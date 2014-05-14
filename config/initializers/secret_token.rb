d0e84688924429f752e4a5e14e474a609d56ba43
Tina <worksametime@hotmail.com>
2014-05-14 21:10:57 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Transformer::Application.config.secret_key_base = '1eca7848c69de7a14b3b49e4c2596f8a8b9431ec952baa6cd36afb6dca7b06399a16efa41b567dd78de7360760cebec65bc2017dfba6f4de991050628b7aff43'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Transformer::Application.config.session_store :cookie_store, key: '_transformer_session'
