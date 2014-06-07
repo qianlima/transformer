ffa01e53a95b213cd2066ff2061ce47a38fccb86
Tina <worksametime@hotmail.com>
2014-06-07 09:14:29 +0000

    edit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,9 +12,15 @@ class PlacesController < ApplicationController
 		current_user.places.create(place_params)
 		redirect_to root_path
 	end
+	
 	def show
-		@place = Place.find(params[:id])
+    @place = Place.find(params[:id])
   	end
+
+  	def edit
+  		@place = Place.find(params[:id])
+  	end
+
 	private
 
 	def place_params
