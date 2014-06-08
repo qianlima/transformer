285331f4595b0d196c22680e2223c0bd7c28e064
Tina <worksametime@hotmail.com>
2014-06-08 04:38:33 +0000

    more validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true
+	validates :name, :presence => true, length: { minimum: 3 }
+	validates :description, length: { maximum: 500 }
+	validates :address, :presence => true
+
 end
